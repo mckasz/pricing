808fa0c28d329d3e6f221c1d4e5e37a50b6e7aa2
Maciej Kaszyński <m.kaszynski@ocado.com>
2019-11-24 14:20:18 +0100

    first task cd
    (cherry picked from commit 1b2de66679c4aa252bb6b5dcde7b6eed7b9dd979)

--- 4-mocking/src/main/java/com/mkaszynski/tdd/pricing/model/SelectedProduct.java
+++ 4-mocking/src/main/java/com/mkaszynski/tdd/pricing/model/SelectedProduct.java
@@ -3,9 +3,11 @@ package com.mkaszynski.tdd.pricing.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Setter
 @Getter
+@ToString
 public class SelectedProduct {
     private String name;
     private int price;
