3234e61dde1c746f4908430962e3672d02ff21e6
Maciej Kaszyński <m.kaszynski@ocado.com>
2019-11-24 16:17:37 +0100

    facade with mocks

--- 5-facade/src/main/java/com/mkaszynski/tdd/pricing/BasketService.java
+++ 5-facade/src/main/java/com/mkaszynski/tdd/pricing/BasketService.java
@@ -2,10 +2,14 @@ package com.mkaszynski.tdd.pricing;
 
 import com.mkaszynski.tdd.pricing.dto.AddProductCommand;
 import com.mkaszynski.tdd.pricing.dto.BasketSummary;
+import lombok.RequiredArgsConstructor;
 
+import static java.util.stream.Collectors.toList;
+
+@RequiredArgsConstructor
 class BasketService {
-    BasketRepository basketRepository;
-    ProductRepository productRepository;
+    private final BasketRepository basketRepository;
+    private final ProductRepository productRepository;
 
     void add(AddProductCommand command) {
         Product product = productRepository.getProduct(command.getName());
@@ -16,7 +20,12 @@ class BasketService {
         basketRepository.save(basket);
     }
 
-    BasketSummary summary(long l) {
-        return null;
+    BasketSummary summary(long basketId) {
+        return new BasketSummary(basketRepository.getBasket(basketId)
+                                                 .products()
+                                                 .stream()
+                                                 .map(SelectedProduct::toSummaryItem)
+                                                 .collect(toList()));
     }
+
 }
--- 5-facade/src/main/java/com/mkaszynski/tdd/pricing/SelectedProduct.java
+++ 5-facade/src/main/java/com/mkaszynski/tdd/pricing/SelectedProduct.java
@@ -1,5 +1,6 @@
 package com.mkaszynski.tdd.pricing;
 
+import com.mkaszynski.tdd.pricing.dto.SummaryItem;
 import com.mkaszynski.tdd.pricing.promotions.Discount;
 import lombok.Value;
 
@@ -45,6 +46,10 @@ public class SelectedProduct {
 
     SelectedProduct addQuantity(int otherQuantity) {
         return new SelectedProduct(name, price, quantity + otherQuantity, type);
+    }
+
+    SummaryItem toSummaryItem() {
+        return new SummaryItem(getName(), getPrice(), quantity());
     }
 
     public enum Type {
