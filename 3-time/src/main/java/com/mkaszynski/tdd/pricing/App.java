47303f26fa917712bc579f6b003b6523b88953cc
Maciej Kaszyński <m.kaszynski@ocado.com>
2019-11-24 12:54:39 +0100

    first task cd
    (cherry picked from commit 8c43f3162c578a0735dbfb91e829ae13791b1c8f)

---
+++ 3-time/src/main/java/com/mkaszynski/tdd/pricing/App.java
@@ -0,0 +1,9 @@
+package com.mkaszynski.tdd.pricing;
+
+class App {
+    public static void main(String[] args) {
+        PromotionHappyHours promotionHappyHours = new PromotionHappyHours(new ProductionTimeProvider());
+        // promotionHappyHours.apply( )
+
+    }
+}
