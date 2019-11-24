9d8dce6c5544ab1c504076f6b457b43396c89b27
Maciej Kaszyński <m.kaszynski@ocado.com>
2019-11-24 16:02:22 +0100

    facade cd

--- 5-facade/src/main/java/com/mkaszynski/tdd/pricing/BasketService.java
+++ 5-facade/src/main/java/com/mkaszynski/tdd/pricing/BasketService.java
@@ -4,8 +4,16 @@ import com.mkaszynski.tdd.pricing.dto.AddProductCommand;
 import com.mkaszynski.tdd.pricing.dto.BasketSummary;
 
 class BasketService {
-    void add(AddProductCommand command) {
+    BasketRepository basketRepository;
+    ProductRepository productRepository;
 
+    void add(AddProductCommand command) {
+        Product product = productRepository.getProduct(command.getName());
+        Basket basket = basketRepository.getBasket(command.getBasketId());
+
+        basket.add(product.toSelectedProduct(command.getQuantity()));
+
+        basketRepository.save(basket);
     }
 
     BasketSummary summary(long l) {
