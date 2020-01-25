7c21d0c3dce2550832a190589c6cd77d5bd2f643
Maciej Kaszyński <m.kaszynski@ocado.com>
2020-01-25 15:05:18 +0100

    PromotionHappyHours time provider

--- 3-time/src/main/java/com/mkaszynski/tdd/pricing/PromotionHappyHours.java
+++ 3-time/src/main/java/com/mkaszynski/tdd/pricing/PromotionHappyHours.java
@@ -1,28 +1,30 @@
 package com.mkaszynski.tdd.pricing;
 
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
 class PromotionHappyHours implements Promotion {
 
     private Discount discount;
+    private TimeProvider timeProvider;
 
-    public PromotionHappyHours(Discount discount) {
+    public PromotionHappyHours(Discount discount, TimeProvider timeProvider) {
         this.discount = discount;
+        this.timeProvider = timeProvider;
     }
 
     // Liquid products should be discounted when it is between 12 and 14
     @Override
     public List<Product> apply(Product product) {
         List<Product> result = new ArrayList<>();
-
-        if (product.type() == Product.Type.LIQUID) {
+        if (product.type() == Product.Type.LIQUID
+                && timeProvider.now().isAfter(LocalTime.parse("12:00"))
+                && timeProvider.now().isBefore(LocalTime.parse("14:00"))) {
             result.add(product.discountedProduct(discount));
         } else {
             result.add(product);
         }
-
         return result;
     }
-
 }
--- 3-time/src/test/java/com/mkaszynski/tdd/pricing/PromotionHappyHoursTest.java
+++ 3-time/src/test/java/com/mkaszynski/tdd/pricing/PromotionHappyHoursTest.java
@@ -3,38 +3,68 @@ package com.mkaszynski.tdd.pricing;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class PromotionHappyHoursTest {
 
-    @DisplayName("Should not apply promotion for FOOD")
+    @DisplayName("Should not apply promotion for FOOD and it is happy hour")
     @Test
     void food() {
-        PromotionHappyHours promotion = promotion();
+        PromotionHappyHours promotion = promotion("12:30");
 
         List<Product> result = promotion.apply(butter());
 
         assertThat(result).containsOnly(butter());
     }
 
-    @DisplayName("Should apply promotion for LIQUID")
+    @DisplayName("Should not apply promotion when it is outside happy hour")
     @Test
-    void liquid() {
-        PromotionHappyHours promotion = promotion();
+    void outsideHappyHours() {
+        PromotionHappyHours promotion = promotion("15:30");
+
+        List<Product> result = promotion.apply(beer());
+
+        assertThat(result).containsOnly(beer());
+    }
+
+    @DisplayName("Should apply promotion for LIQUID when it is HappyHour")
+    @Test
+    void liquidAndHappyHour() {
+        PromotionHappyHours promotion = promotion("12:30");
 
         List<Product> result = promotion.apply(beer());
 
         assertThat(result).containsOnly(discountedBeer());
     }
 
-    private PromotionHappyHours promotion() {
-        return new PromotionHappyHours(new Discount(30));
+    @DisplayName("Should not apply promotion for FOOD when it is outside HappyHour")
+    @Test
+    void foodAndNotHappyHour() {
+        PromotionHappyHours promotion = promotion("15:30");
+
+        List<Product> result = promotion.apply(butter());
+
+        assertThat(result).containsOnly(butter());
+    }
+
+    private PromotionHappyHours promotion(final String currentTime) {
+        return new PromotionHappyHours(new Discount(30), new TimeProvider() {
+            @Override
+            public LocalTime now() {
+                return LocalTime.parse(currentTime);
+            }
+        });
     }
 
     private static Product butter() {
-        return new Product("butter", 220, 1, Product.Type.FOOD);
+        return new Product("butter", 1000, 1, Product.Type.FOOD);
+    }
+
+    private static Product discountedButter() {
+        return new Product("butter", 700, 1, Product.Type.FOOD);
     }
 
     private static Product beer() {
